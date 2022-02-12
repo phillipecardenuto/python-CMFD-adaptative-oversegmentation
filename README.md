7a1abb44972ce3538424a40484364efa8b5c51cd
João Phillipe <phillipe.cardenuto@ic.unicamp.br>
2022-02-12 10:25:49 -0300

    upload to github

---
+++ README.md
@@ -0,0 +1,21 @@
+Image forgery detection using adaptive over-segmentation and feature points matching
+
+Here you will find a python implementation of Pun et al.  [1].
+
+
+
+---
+
+We base our implementation in the Pun et al. article and in the MatLab code from the github user MohenseZandi [2]
+
+If you use any code from this repo, cite and give the credits to the original authors:
+
+```
+[1] Pun, C., Yuan, X. and Bi, X., 2015. Image Forgery Detection Using Adaptive Oversegmentation and Feature Point Matching. IEEE Transactions on Information Forensics and Security, 10(8), pp.1705-1716.
+[2] Github Repository: https://github.com/MohsenZandi/Copy-Move_Forgery_Detection/tree/master/Image%20forgery%20detection%20using%20adaptive%20over-segmentation%20and%20feature%20points%20matching (Last Access, Feb 2022)
+```
+
+----
+
+
+
---
+++ python/run_cmfd.py
@@ -0,0 +1,58 @@
+from oversegmentation import *
+import matplotlib.pyplot as plt
+from glob import glob
+from tqdm.contrib.concurrent import process_map
+from pathlib import Path
+from PIL import Image
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import cv2
+import os
+
+files = glob("/work/jcardenuto/translate-forgery-algs/testset/**/figure_v?.png", recursive=True)
+files += glob("/work/jcardenuto/translate-forgery-algs/testset/**/figure.png", recursive=True)
+
+def run_method(fname):
+    exist_name = fname.split('testset/')[-1]
+    exist_name = exist_name[:-4]
+    if os.path.isfile(f"result/{exist_name}_map.png")  \
+            and os.path.isfile(f"result/{exist_name}_final_map.png") \
+            and os.path.isfile(f'result/{exist_name}_final_map.png') \
+            and os.path.isfile(f'result/{exist_name}_matched_image.png'):
+        return
+
+    #img = cv2.imread(fname)
+    #img_rgb = img[...,::-1]
+    img_rgb = Image.open(fname)
+    img_rgb = np.array(img_rgb)
+
+    # run method 
+    forgery_map , matches, Locations = oversegmentation("r30_s1200mask.png")
+
+    # Fix locations
+    p1 = []
+    p2 = []
+    for match in matches:
+        point1, point2 = match
+        p1.append( tuple( int(p) for p in Locations[point1][1::-1]))
+        p2.append( tuple( int(p) for p in Locations[point2][1::-1]))
+    p1 = np.array(p1).T
+    p2 = np.array(p2).T
+
+    final_map = generated_object_match_map(forgery_map,p1, p2)
+
+    fname = fname.split('testset/')[-1]
+    fname = fname[:-4]
+    os.makedirs(f'result/{fname}',exist_ok=True)
+
+    #np.save(f"result/{fname}_map.npy", map)
+    map = map.astype("uint8")
+    #np.save(f"result/{fname}_final_map.npy", final_map)
+    final_map = final_map.astype("uint8")
+    cv2.imwrite(f"result/{fname}_map.png",map)
+    cv2.imwrite(f"result/{fname}_final_map.png", final_map)
+    plt.imsave(f'result/{fname}_final_map.png', final_map)
+
+
+process_map(run_method, files, chunksize = 10, max_workers = 30)
+
