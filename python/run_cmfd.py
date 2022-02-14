e9a245dfed54a4b356cf355195bb2e7fae244f77
João Phillipe <phillipe.cardenuto@ic.unicamp.br>
2022-02-14 20:57:11 -0300

    fix module name

--- python/run_cmfd.py
+++ python/run_cmfd.py
@@ -1,5 +1,5 @@
 from oversegmentation import *
-import matplotlib.pyplot as plt
+from matching_postprocessing import generated_object_match_map
 from glob import glob
 from tqdm.contrib.concurrent import process_map
 from pathlib import Path
@@ -9,8 +9,9 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 import cv2
 import os
 
-files = glob("/work/jcardenuto/translate-forgery-algs/testset/**/figure_v?.png", recursive=True)
-files += glob("/work/jcardenuto/translate-forgery-algs/testset/**/figure.png", recursive=True)
+#files = glob("/work/jcardenuto/translate-forgery-algs/testset/**/figure_v?.png", recursive=True)
+#files += glob("/work/jcardenuto/translate-forgery-algs/testset/**/figure.png", recursive=True)
+files = glob("*.png")
 
 def run_method(fname):
     exist_name = fname.split('testset/')[-1]
@@ -27,7 +28,7 @@ def run_method(fname):
     img_rgb = np.array(img_rgb)
 
     # run method 
-    forgery_map , matches, Locations = oversegmentation("r30_s1200mask.png")
+    forgery_map , matches, Locations = oversegmentation(fname)
 
     # Fix locations
     p1 = []
@@ -45,14 +46,11 @@ def run_method(fname):
     fname = fname[:-4]
     os.makedirs(f'result/{fname}',exist_ok=True)
 
-    #np.save(f"result/{fname}_map.npy", map)
-    map = map.astype("uint8")
-    #np.save(f"result/{fname}_final_map.npy", final_map)
+    forgery_map = forgery_map.astype("uint8")
     final_map = final_map.astype("uint8")
-    cv2.imwrite(f"result/{fname}_map.png",map)
+    cv2.imwrite(f"result/{fname}_map.png",forgery_map)
     cv2.imwrite(f"result/{fname}_final_map.png", final_map)
     plt.imsave(f'result/{fname}_final_map.png', final_map)
-
 
 process_map(run_method, files, chunksize = 10, max_workers = 30)
 
